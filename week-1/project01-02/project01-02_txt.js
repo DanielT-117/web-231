077850c26626b8a7541b8ff151f94dd0958a5b37
Daniel <dtopete011@gmail.com>
2026-08-13 10:07:04 -0700

    Added and completed project01-02 files

---
+++ week-1/project01-02/project01-02_txt.js
@@ -0,0 +1,11 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Daniel Topete
+    Date: 8/13/2026
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
