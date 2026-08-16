8303b00adb13d240514e48b8bec3b6d4b9e7f194
Daniel <dtopete011@gmail.com>
2026-08-16 14:26:16 -0700

    Added week-2 folders and completed week-2 chapter code-along

---
+++ week-2/chapter/js02_txt.js
@@ -0,0 +1,12 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: Daniel Topete
+      Date:8/16/2026
+
+      Filename: js02.js
+ */
+
