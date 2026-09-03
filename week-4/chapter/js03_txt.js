63c9845cd76cec8cea3e46db86a4f201a51f0ef7
Daniel <dtopete011@gmail.com>
2026-09-03 13:07:04 -0700

    Completed chapters code-along

--- week-4/chapter/js03_txt.js
+++ week-4/chapter/js03_txt.js
@@ -4,9 +4,11 @@
 
      Tipton Turbines
      Program to display games results in a web table
-     Author: 
-     Date:   
+     Author: Daniel Topete
+     Date: 9/3/2026
 
      Filename: js03.js
  */
+// Days of the week
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
